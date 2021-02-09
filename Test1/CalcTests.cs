cd2a3e3e9b8c88daacb0316b908438d31ce2ae74
VeronicaRuizJ <bethzabe@gmail.com>
2021-02-09 12:10:31 -0600

    Agregar archivos de proyecto.

---
+++ Test1/CalcTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitTestingMsTest1;
+
+
+namespace Test1
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void Sum_ShouldCalcAndReturnSameValue()
+        {
+            //Arrange 
+
+            int a = 4;
+            int b = 4;
+            int expected = 8;
+
+            //Act
+
+            int actual = Calcs.Sum(a, b);
+
+            //Assert
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void Multiply_ShoulCalAndReturnSameValue()
+        {
+            //Arrange
+            int a = 6;
+            int b = 10;
+            int expect = 50;
+
+            //Act
+
+            int actual = Calcs.Multiply(a,b);
+
+            //Assert
+
+            Assert.AreEqual(expect, actual);
+
+
+        }
+
+    }
+}
---
+++ UnitTestingMsTest1/Calcs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestingMsTest1
+{
+    public class Calcs
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
